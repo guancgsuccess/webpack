ad7ffca135ab1e4818e9317f24bf7435bf0cd6f5
success <849962874@qq.com>
2018-12-27 13:52:33 +0800

    loaders实现处理图片以及分离css

--- code/webpack_demo07/src/index01.js
+++ code/webpack_demo07/src/index01.js
@@ -1,2 +1,8 @@
 import '../src/success.css'
+//import imgSrc from './imgs/school_all.png';
+//let oImg = new Image();
+//oImg.onload = function(){
+//  document.body.appendChild(oImg);
+//};
+//oImg.src = imgSrc;
 document.write("loaders...")
--- code/webpack_demo07/webpack.config.js
+++ code/webpack_demo07/webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin') 
 //const cleanWebpackPlugin = require('clean-webpack-plugin')
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 module.exports={
 	//入口配置
 	entry:{
@@ -24,7 +26,21 @@ module.exports={
 				//利用正则表示所有以.css后缀的样式文件
 				test:/\.css$/,
 				//使用'style-loader','css-loader'
-				use:['style-loader','css-loader']
+				//use:['style-loader','css-loader']
+				use:ExtractTextPlugin.extract({
+					fallback:'style-loader',//回滚
+					use:'css-loader',
+					publicPath:'../' //解决背景图丢失问题
+				})
+			},{
+				test:/\.(png|jpg|gif)$/,
+				use:{
+					loader:'url-loader',
+					options:{// 这里的options选项参数可以定义多大的图片转换为base64
+						limit:150000, // 表示小于150kb的图片转为base64,大于150kb的是路径
+						outputPath:'imgs'
+					}
+				}
 			}
 		]
 	},
@@ -42,6 +58,8 @@ module.exports={
 		//hotOnly:true
 	}
 	,plugins:[
+		//在plugins中调用插件(配置提出来的css名称以及提到哪里)
+		new ExtractTextPlugin('css/success.css'),
 		//热更新加载
 		new webpack.HotModuleReplacementPlugin(),
 		//new cleanWebpackPlugin(['dist']),
